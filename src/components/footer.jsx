2cca4e927f0bc140d11f6f5d81f3001c0ee5c4d0
Jhon Martin <martinjhon@gmail.com>
2024-03-26 18:36:09 -0500

    Actualizaciones

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,8 +3,11 @@ export default function Footer(){
 
     return (
         <div className="bg-gray-800 text-white py-4 text-center bottom-0 w-full">
-            <h1>Proyecto talento Tech</h1>
-            <h2 className="text-red-500">Desarrollador por Jhon Martin</h2>
+            <h1>Proyecto Talento Tech</h1>
+            <h2 className="text-red-500">Desarrollado por Jhon Martin</h2>
+            <p className="text-white-800 mt-4">
+            © 2024 Proyecto Talento Tech - Todos los derechos reservados
+            </p>
         </div>
     );
 }
